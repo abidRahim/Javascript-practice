104ac62217d889520af82a71e699363069d57bee
abidRahim <abid.rahim1@gmail.com>
2018-12-10 10:33:55 +0530

    Day 14 assignment start

---
+++ Day-wise-AltCampus-javascript-projects/Day 14 - HOF, Callbacks and Closure/projects/1/main.js
@@ -0,0 +1,106 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log('Hello, world!');
+
+
+// Challenge 1
+function addTwo(num) {
+  return num + 2;
+}
+
+// To check if you've completed it, uncomment these console.logs!
+console.log(addTwo(3));
+console.log(addTwo(10));
+
+
+// Challenge 2
+function addS(word) {
+  return word + "s";
+}
+
+// uncomment these to check your work
+console.log(addS('pizza'));
+console.log(addS('bagel'));
+
+
+// Challenge 3
+function map(array, callback) {
+  return array.map( val => callback(val));
+}
+
+console.log(map([1, 2, 3], addTwo));
+
+
+// Challenge 4
+function forEach(array, callback) {
+  array.forEach(val => console.log(callback(val)));
+}
+
+// see for yourself if your forEach works!
+
+
+//--------------------------------------------------
+// Extension
+//--------------------------------------------------
+
+//Extension 1
+function mapWith(array, callback) {
+  var newArr = [];
+
+  array.forEach( val => newArr = callback(val));
+
+  return newArr;
+}
+
+//Extension 2
+function reduce(array, callback, initialValue) {
+  array.forEach( val => initialValue = callback(initialValue, val));
+  return initialValue;
+}
+
+function add(a, b) {
+   return a + b;
+}
+
+//Extension 3
+  function intersection(array) {
+    let args = [...arguments];
+    var filteredArr = [];
+    
+    return args.reduce( (acc, cur, i) => {
+      
+      acc.forEach( val => {
+        filteredArr = cur.filter( v => v == val );
+      });
+      acc = [];
+      
+      return acc.push(filteredArr);
+    }, args[0]);
+  }
+
+// console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+// should log: [5, 15]
+
+//Extension 4
+function union(arrays) {
+
+}
+
+// console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+// should log: [5, 10, 15, 88, 1, 7, 100]
+
+//Extension 5
+function objOfMatches(array1, array2, callback) {
+
+}
+
+// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+//Extension 6
+function multiMap(arrVals, arrCallbacks) {
+
+}
+
+// console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
