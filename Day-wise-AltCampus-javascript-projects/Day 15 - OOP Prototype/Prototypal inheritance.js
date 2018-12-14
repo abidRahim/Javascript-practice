497b29bfe4466c4c47c3fa128b3967256acb9145
abidRahim <abid.rahim1@gmail.com>
2018-12-14 21:34:07 +0530

    Day 15 OOPs

---
+++ Day-wise-AltCampus-javascript-projects/Day 15 - OOP Prototype/Prototypal inheritance.js
@@ -0,0 +1,162 @@
+// 1. Factory function //
+
+  var studFunctions = {
+    percentage: function() {
+      let avg = (this.phy + this.chem + this.math) / 3;
+      let perAge = (avg / 300) * 100;
+      return perAge;
+    },
+
+    passOrFail: function() {
+      let avg = (this.phy + this.chem + this.math) / 3;
+      let perAge = (avg / 300) * 100;
+      return (perAge >= 40) ? "PASS" : "FAIL;"
+    }
+  }
+
+  function studCreator(p, c, m) {
+    var obj = Object.create(studFunctions);
+
+    obj.math = m;
+    obj.phy = p;
+    obj.chem = c;
+
+    return obj;
+  }
+
+  var abid = studCreator(90, 80, 70);
+
+
+  // ---------------------------
+
+  var empFunctions = {
+    avgHours: function() {
+      let hoursSpent = this.hours / 26;
+      return hoursSpent;
+    }
+  }
+  var salesFuncitons = {
+    targetAchieved: function() {
+      if(this.target == this.sales) return true;
+      else false;
+    }
+  }
+
+
+  function salesCreator(name, id, target, sales, hours) {
+    var obj = Object.create(salesFuncitons);
+    var obj2 = Object.create(empFunctions);
+
+    obj.target = target;
+    obj.sales = sales;
+    
+    obj2.name = name;
+    obj2.id = id;
+    obj2.hours = hours;
+
+    Object.setPrototypeOf(obj, obj2 );
+
+    return obj;      
+  }
+
+  // var batch1 = salesCreator( "kiran", 404, 20, 20, 200);
+
+// 2. Psuedo Classical //
+
+  function StudentMarks(p, c, m) {
+    this.phy = p;
+    this.chem = c;
+    this.math = m;
+  }
+  
+  studentMarks.prototype.percentage = function() {
+    let avg = (this.phy + this.chem + this.math) / 3;
+    let perAge = (avg / 300) * 100;
+    return perAge;
+  }
+
+  studentMarks.prototype.total = function() {
+    let tot = this.phy + this.chem + this.math;
+    return tot;
+  }
+
+  studentMarks.prototype.passOrFail = function() {
+    let avg = (this.phy + this.chem + this.math) / 3;
+    let perAge = (avg / 300) * 100;
+    return (perAge >= 40) ? "PASS" : "FAIL;"
+  }
+
+  var abid = new studentMarks(40, 50, 60);
+  var rishi = new studentMarks(60, 70, 80);
+
+  abid.percentage();
+  .percentage();
+
+
+// 3. Classes //
+
+  class studCreator {
+    constructor(p, c, m) {
+
+      this.phy = p;
+      this.chem = c;
+      this.math = m;
+    }
+
+    percentage() {
+      let perAge = (this.phy + this.chem + this.math) / 3;
+      return perAge;
+    }
+  
+    total() {
+      let tot = this.phy + this.chem + this.math;
+      return tot;
+    }
+  
+    passOrFail() {
+      let perAge = (this.phy + this.chem + this.math) / 3;
+      return (perAge >= 40) ? "PASS" : "FAIL;"
+    }
+
+    isStudent = true;
+  }
+
+  var abid = new studCreator(90, 80, 70);
+
+
+
+  // ------------------------------------------------------
+
+
+  var empFunctions = {
+    avgHours: function() {
+      let hoursSpent = this.hours / 26;
+      return hoursSpent;
+    }
+  }
+  var salesFuncitons = {
+    targetAchieved: function() {
+      if(this.target == this.sales) return true;
+      else false;
+    }
+  }
+
+
+  function salesCreator(name, id, target, sales, hours) {
+    var obj = Object.create(salesFuncitons);
+
+    var obj2 = Object.create(empFunctions);
+
+    obj.target = target;
+    obj.sales = sales;
+    
+    obj2.name = name;
+    obj2.id = id;
+    obj2.hours = hours;
+
+    Object.setPrototypeOf(obj,obj2);
+
+    return obj;      
+  }
+
+  // ------------------------------------------------------------------------------ //
---
+++ Day-wise-AltCampus-javascript-projects/Day 15 - OOP Prototype/projects/2.js
@@ -0,0 +1,24 @@
+// Define a constructor function called Persone which takes three arguments
+//(name, yearOfBirth, job) Initiate the properties
+
+function Persone(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+
+
+// Define a function name calculateAge which returns the age of the person
+
+function calculateAge() {
+  
+}
+
+// Use constructor function to make three objects with different values for example ('John', 1990, 'teacher')
+
+
+
+// call the calculateAge function on each object
+
+
